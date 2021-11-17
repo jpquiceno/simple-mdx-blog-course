c994bef428b4bda3acee1f3528eb09e1a5853cf0
JPQuiceno <Jean-Paul.Quiceno@coxautoinc.com>
2021-11-17 15:28:37 -0500

    Added google fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,6 +37,16 @@ module.exports = {
           }
         ]
       }
-    }
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `roboto mono`,
+          `muli\:400, 400i, 700, 700i`
+        ],
+        display: "swap",
+      }
+    },
   ],
 }
